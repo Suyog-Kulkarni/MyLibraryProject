a9ff3f9276ad66d32040f82514b3fa6575c659ad
Suyog Kulkarni <kulkarnisuyog192@gmail.com>
2024-06-05 19:42:21 +0530

    Add project files.

---
+++ MyLibrary.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MyLibrary.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ MyLibrary.Tests/BaseTest.cs
@@ -0,0 +1,51 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
+using System.Security.Policy;
+
+namespace MyLibrary.Tests
+{
+    public class BaseTest
+    {
+        protected ExtentReports extent;
+        protected ExtentTest test;
+
+        [OneTimeSetUp]
+        public void BeforeClass()
+        {
+            extent = new ExtentReports();
+            var htmlReporter = new ExtentHtmlReporter("TestReport.html");
+            extent.AttachReporter(htmlReporter);
+        }
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stackTrace = TestContext.CurrentContext.Result.StackTrace;
+
+            if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            {
+                test.Fail(stackTrace);
+            }
+            else if (status == NUnit.Framework.Interfaces.TestStatus.Passed)
+            {
+                test.Pass("Test passed");
+            }
+        }
+
+        [OneTimeTearDown]
+        public void AfterClass()
+        {
+            extent.Flush();
+        }
+    }
+}
+has context menu
+
